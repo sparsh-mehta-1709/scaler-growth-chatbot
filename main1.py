64a77cff4d48024907b801d8b8f45d43b3702d3a
sparsh-mehta-1709 <sparsh.mehta@scaler.com>
2024-09-12 02:03:43 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -320,8 +320,7 @@ case when payment_flag=1 then paying_for_type end as paid_for_program,
 prospectstage as current_stage
 from temp.non_marketing_mis
     """
-
-
+28. For temp.marketing_mis use case when first_payment_done=0 then 'Payment Not Done' when first_payment_done=1 then 'Payment Done' end as first_payment_done
     prompt = f"""Given the following reference logic and conversation history:
 
 Reference Logic:
